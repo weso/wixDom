a03d6fad14ce88846403a48e1bf952f21845a7f7
DaniFdezAlvarez <danifdezalvarez@gmail.com>
2014-11-04 13:24:45 +0100

    Stablish skeleton of the service

--- webindex/domain/services/rdf_service.py
+++ webindex/domain/services/rdf_service.py
@@ -1,16 +1,24 @@
 import rdflib
-from infrastructure.mongo_repos.observation_repository import ObservationRepository
 
 __author__ = 'miguel'
 
 
 class RDFService(object):
 
-    def __init__(self):
-        self._obs_repo = ObservationRepository("127.0.0.1")
+    def __init__(self, area_repository, indicator_repository):
+        self._area_repository = area_repository
+        self._indicator_repository = indicator_repository
 
-    def generate_dataset(self):
-        observations_document = self._obs_repo.find_observations()
-        if not observations_document["success"]:
-            return
+    def generate_dataset(self, graph, observations):
+        for observation in observations:
+            self._process_observation(observation, graph)
+        return graph  # We will see if this is the thing to return or not
+
+    def _process_observation(self, observation, graph):
+        self._add_country_triplets(observation, graph)
+        self._add_indicator_triplets(observation, graph)
+        self._add_observation_triplets(observation, graph)
+
+
+
 
