32a7a0965adf38e0771661dc811e73c76d470022
Miguel Otero <otemi89@gmail.com>
2014-11-04 14:07:56 +0100

    Add first prefixes

---
+++ webindex/domain/services/namespaces_handler.py
@@ -0,0 +1,38 @@
+__author__ = 'guillermo'
+
+from rdflib import Namespace, URIRef
+
+# Namespaces
+cex = Namespace("http://purl.org/weso/computex/ontology#")
+dct = Namespace("http://purl.org/dc/terms/")
+dctype = Namespace("http://purl.org/dc/dcmitype/")
+foaf = Namespace("http://xmlns.com/foaf/0.1/")
+lb = Namespace("http://purl.org/weso/landbook/ontology#")
+org = Namespace("http://www.w3.org/ns/org#")
+qb = Namespace("http://purl.org/linked-data/cube#")
+sdmx_concept = Namespace("http://purl.org/linked-data/sdmx/2009/concept#")
+time = Namespace("http://www.w3.org/2006/time#")
+sdmx_code = Namespace("http://purl.org/linked-data/sdmx/2009/code#")
+
+base = Namespace("http://data.webfoundation.org/webindex/v2013/")
+base_time = Namespace("http://data.webfoundation.org/webindex/v2013/time/")
+base_obs = Namespace("http://data.webfoundation.org/webindex/v2013/observation/")
+base_ind = Namespace("http://data.webfoundation.org/webindex/v2013/indicator/")
+base_area = Namespace("http://data.webfoundation.org/webindex/v2013/area/")
+base_org = Namespace("http://data.webfoundation.org/webindex/v2013/organization/")
+
+dcat = Namespace("http://www.w3.org/ns/dcat#")
+
+
+def bind_namespaces(graph):
+    """
+    Binds Landportal uris with their corresponding prefixes
+    """
+    n_space = {"cex": cex, "dct": dct, "dctype": dctype, "foaf": foaf,
+               "lb": lb, "org": org, "qb": qb, "sdmx-concept": sdmx_concept, "time": time,
+               "sdmx-code": sdmx_code, "": base, "base-obs": base_obs,
+               "base-ind": base_ind, "base-org": base_org, "base-time": base_time,
+               "dcat": dcat}
+
+    for prefix, uri in n_space.items():
+        graph.namespace_manager.bind(prefix, URIRef(Namespace(uri)))
