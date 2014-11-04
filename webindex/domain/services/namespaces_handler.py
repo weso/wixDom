194f6d31cf7febc8654983e56155f683905489f0
miguel-otero <otemi89@gmail.com>
2014-11-04 19:07:18 +0100

    Add indicator triples

--- webindex/domain/services/namespaces_handler.py
+++ webindex/domain/services/namespaces_handler.py
@@ -7,26 +7,26 @@ cex = Namespace("http://purl.org/weso/computex/ontology#")
 dct = Namespace("http://purl.org/dc/terms/")
 dctype = Namespace("http://purl.org/dc/dcmitype/")
 foaf = Namespace("http://xmlns.com/foaf/0.1/")
-lb = Namespace("http://purl.org/weso/landbook/ontology#")
+lb = Namespace("http://purl.org/weso/landbook/ontology#") # Check this
 org = Namespace("http://www.w3.org/ns/org#")
 qb = Namespace("http://purl.org/linked-data/cube#")
 sdmx_concept = Namespace("http://purl.org/linked-data/sdmx/2009/concept#")
 time = Namespace("http://www.w3.org/2006/time#")
 sdmx_code = Namespace("http://purl.org/linked-data/sdmx/2009/code#")
 
-base = Namespace("http://data.webfoundation.org/webindex/v2013/")
-base_time = Namespace("http://data.webfoundation.org/webindex/v2013/time/")
-base_obs = Namespace("http://data.webfoundation.org/webindex/v2013/observation/")
-base_ind = Namespace("http://data.webfoundation.org/webindex/v2013/indicator/")
-base_area = Namespace("http://data.webfoundation.org/webindex/v2013/area/")
-base_org = Namespace("http://data.webfoundation.org/webindex/v2013/organization/")
+base = Namespace("http://data.webfoundation.org/webindex/v2014/")
+base_time = Namespace("http://data.webfoundation.org/webindex/v2014/time/")
+base_obs = Namespace("http://data.webfoundation.org/webindex/v2014/observation/")
+base_ind = Namespace("http://data.webfoundation.org/webindex/v2014/indicator/")
+base_area = Namespace("http://data.webfoundation.org/webindex/v2014/area/")
+base_org = Namespace("http://data.webfoundation.org/webindex/v2014/organization/")
 
 dcat = Namespace("http://www.w3.org/ns/dcat#")
 
 
 def bind_namespaces(graph):
     """
-    Binds Landportal uris with their corresponding prefixes
+    Binds Webindex uris with their corresponding prefixes
     """
     n_space = {"cex": cex, "dct": dct, "dctype": dctype, "foaf": foaf,
                "lb": lb, "org": org, "qb": qb, "sdmx-concept": sdmx_concept, "time": time,
--- webindex/domain/services/rdf_service.py
+++ webindex/domain/services/rdf_service.py
@@ -1,6 +1,6 @@
-# import rdflib
 from namespaces_handler import *
-from rdflib.namespace import RDF
+from rdflib.namespace import RDF, RDFS
+from rdflib import Literal
 
 __author__ = 'miguel'
 
@@ -20,11 +20,10 @@ class RDFService(object):
         #The next two dicts will be used to check if the triples of certain country/indicator
         #had already been added to the graph. It will make a faster execution.
         countries_added_dict = {}
-        indicatros_added_dict = {}
+        indicators_added_dict = {}
         self._add_country_triples(observation, graph, countries_added_dict)
-        self._add_indicator_triples(observation, graph, indicatros_added_dict)
+        self._add_indicator_triples(observation, graph, indicators_added_dict)
         self._add_observation_triples(observation, graph)
-
 
     def _add_country_triples(self, observation, graph, countries_dict):
         if observation['area'] in countries_dict:
@@ -42,14 +41,38 @@ class RDFService(object):
 
         ### [And the rest os possible country triples]
 
-
-
+    def _add_indicator_triples(self, observation, graph, indicators_dict):
+        if observation['indicator'] in indicators_dict:
+            return
+        indicators_dict[observation['indicator']] = True
+        indicator_dict = self._find_indicator_dict(observation['indicator'])
+        indicator_term = base_ind.term(observation['indicator'])
+        graph.add(indicator_term, RDF.type, cex.term("Indicator"))
+        graph.add(indicator_term, RDFS.label, Literal(indicator_dict['name'], lang='en'))
+        graph.add(indicator_term, RDFS.comment, Literal(indicator_dict['description'], lang='en'))
+        graph.add(indicator_term, lb.term('indicatorType'), cex.term(indicator_dict['type'])) # Check this.
+                                                                                              # Is it lb for sure?
+        # graph.add((base_ind.term(ind.id), lb.term("measurement"),
+        #            Literal(ind.measurement_unit.name)))
+        # graph.add((base_ind.term(ind.id), lb.term("last_update"),
+        #            Literal(self.time.strftime("%Y-%m-%d"), datatype=XSD.date)))
+        # graph.add((base_ind.term(ind.id), lb.term("starred"),
+        #            Literal(ind.starred, datatype=XSD.Boolean)))
+        # graph.add((base_ind.term(ind.id), lb.term("topic"),
+        #            base_topic.term(ind.topic_id)))
+        return graph
 
     def _find_country_dict(self, iso3_code):
         result = self._area_repository.find_countries_by_code_or_income(iso3_code)['data']
         if result is None:
             raise ValueError("Unknown country while trying to add rdf triplets: {}".format(iso3_code))
         return result
+
+    def _find_indicator_dict(self, indicator_code):
+        result = self._indicator_repository.find_indicators_by_code(indicator_code)
+        if not result['success'] or result['data'] is None:
+            raise ValueError("Unknown indicator while trying to add rdf triplets: {}".format(indicator_code))
+        return result['data']
 
 
 
